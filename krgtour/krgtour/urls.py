85540053a0d87d407e08389c4f3dfe43a852a252
sakenzi <ermekulysaken8@gmail.com>
2026-02-08 18:26:31 +0500

    add auth page

--- krgtour/krgtour/urls.py
+++ krgtour/krgtour/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("routes.urls")),
     path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/", include("accounts.urls")),
 ]
